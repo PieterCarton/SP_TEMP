705c3c335e218ec911830cecd60c1bab656768e1
Pieter Carton <pieterc.n@hotmail.com>
2022-04-21 11:50:57 +0200

    add test coverage tool

--- src/mirte_sensor_lib.js
+++ src/mirte_sensor_lib.js
@@ -2,4 +2,6 @@ function isEven (num) {
   return num % 2 === 0;
 }
 
-module.exports = isEven;
+module.exports = {
+  isEven: isEven
+};
--- test/unit/mirte_sensor_lib.test.js
+++ test/unit/mirte_sensor_lib.test.js
@@ -1,8 +1,16 @@
 var assert = require('assert');
+var sensorLib = require('../../src/mirte_sensor_lib.js');
+
 describe("Mirte Sensor Library", function () {
   describe("#isEven(num)", function () {
     it('should return true for even numbers', function () {
-      assert.equal(1, 1);
+      assert.equal(sensorLib.isEven(2), true);
+    });
+    it('should return false for odd numbers', function () {
+      assert.equal(sensorLib.isEven(1), false);
+    });
+    it('should return false for non-numbers', function () {
+      assert.equal(sensorLib.isEven("hello"), false);
     });
   });
 });
